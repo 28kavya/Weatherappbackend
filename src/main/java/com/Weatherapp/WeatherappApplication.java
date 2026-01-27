07905135cec4acc485b7f06920530f50bd6c544e
Kavya <skavyasarav@gmail.com>
2026-01-27 19:39:33 +0530

    Updated

--- src/main/java/com/Weatherapp/WeatherappApplication.java
+++ src/main/java/com/Weatherapp/WeatherappApplication.java
@@ -11,8 +11,4 @@ public class WeatherappApplication {
 	public static void main(String[] args) {
         SpringApplication.run(WeatherappApplication.class, args);
     }
-        @Bean
-        public RestTemplate restTemplate () {
-            return new RestTemplate();
-        }
     }

a35c08d0c62ebc14cfe4eb4cfa8b51bd167e4b4e
Kavya <skavyasarav@gmail.com>
2026-01-27 19:31:47 +0530

    Updated

--- src/main/java/com/Weatherapp/WeatherappApplication.java
+++ src/main/java/com/Weatherapp/WeatherappApplication.java
@@ -2,15 +2,17 @@ package com.Weatherapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class WeatherappApplication {
 
 	public static void main(String[] args) {
         SpringApplication.run(WeatherappApplication.class, args);
-
+    }
         @Bean
         public RestTemplate restTemplate () {
             return new RestTemplate();
         }
-    }}
+    }
